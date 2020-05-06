c007a4e73e2099b1eb20463faea837d21546d48b
Aleksandr Lundin <aslundin@ya.ru>
2020-05-06 23:48:30 +0300

    first commit

---
+++ convos-shortcode.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Plugin Name: Custom ShortCodes by Alex Lundin
+ * Author:      Alex Lundin
+ * Version:     1.1.1
+ * Description: Custom Shortcodes for text
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://alexlundin.com/wp-update-server?action=get_metadata&slug=custom-shortcode-by-alex-lundin', //Metadata URL.
+    __FILE__, //Full path to the main plugin file.
+    'custom-shortcode-by-alex-lundin' //Plugin slug. Usually it's the same as the name of the directory.
+);
+
+add_shortcode("plus_h", function ($args = null, $content = null) {
+    return "<div class='h3'>✅ {$content}</div>";
+});
+add_shortcode("minus_h", function ($args = null, $content = null) {
+    return "<div class='h3'>⛔ {$content}</div>";
+});
+add_shortcode("gray", function ($args = null, $content = null) {
+    return "<div class='gray_table'> {$content} </div>";
+});
+add_shortcode("purple_bd", function ($args = null, $content = null) {
+    return "<span class='p_bd'> {$content} </span>";
+});
+add_shortcode("purple_bg", function ($args = null, $content = null) {
+    return "<span class='p_bg'> {$content} </span>";
+});
+add_shortcode("gradient_bd", function ($args = null, $content = null) {
+    return "<span class='p_bd ng_bd'> {$content} </span>";
+});
+add_shortcode("gradient_bg", function ($args = null, $content = null) {
+    return "<span class='p_bg ng_bg'> {$content} </span>";
+});
+add_shortcode("gradient_color", function ($args = null, $content = null) {
+    return "<span class='gr_color'> {$content} </span>";
+});
+
+add_action('wp', 'add_css');
+
+function add_css()
+{
+    /* Получаем глобальную переменную $post */
+    global $post;
+
+    /* Регистрируем таблицу стилей */
+    wp_register_style('customShortcodes', plugin_dir_url(__FILE__) . 'css/styles.css');
+
+
+    /* Проверяем нет присутствует ли в записи шорткод, если да то выводит css */
+    if ( has_shortcode($post->post_content, 'plus_h') ||
+         has_shortcode($post->post_content, 'minus_h') ||
+         has_shortcode($post->post_content, 'gray') ||
+         has_shortcode($post->post_content, 'purple_bd') ||
+         has_shortcode($post->post_content, 'purple_bg') ||
+         has_shortcode($post->post_content, 'gradient_bd') ||
+         has_shortcode($post->post_content, 'gradient_bg') ||
+         has_shortcode($post->post_content, 'gradient_color')
+    ) {
+        wp_enqueue_style('customShortcodes');
+    }
+}
+
+
+add_action("admin_enqueue_scripts", function () {
+    wp_enqueue_script("custom_shortcode_btn", plugin_dir_url(__FILE__) . 'js/custom_short_btns.js', array('wp-tinymce-root'), null , true);
+});
+add_action( 'admin_init', function () {
+    if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
+        add_filter( 'mce_buttons_3', function ($buttons) {
+            array_push( $buttons, "minus", "plus", "gray", "custom_table", "purple_background", "purple_border", "gradient_background", "gradient_border", "light_gradient");
+            return $buttons;
+        });
+        add_filter( 'mce_external_plugins', function ($plugin_array) {
+            $plugin_array['custom_shortcodes'] =  plugin_dir_url(__FILE__) .'js/custom_short_btns.js';
+            return $plugin_array;
+        });
+    }
+} );
+
+
+
---
+++ js/custom_short_btns.js
@@ -0,0 +1,151 @@
+(function () {
+
+	let url = window.location.hostname;
+	let path = '/wp-content/plugins/custom-shortcode-by-alex-lundin/img/';
+	let full = '//' + url + path;
+
+    tinymce.create('tinymce.plugins.CustomShort', {
+        init : function(editor, url) {
+            editor.addButton('plus', {
+                image: full + 'head_plus_icon.png',
+                title: 'Цитата с плюсом',
+                onclick: function () {
+                    editor.insertContent('[plus_h]' + editor.selection.getContent() + '[/plus_h]');
+                }
+            });
+            editor.addButton('minus', {
+                image: full + 'head_minus_icon.png',
+                title: 'Цитата с минусом',
+                onclick: function () {
+                    editor.insertContent('[minus_h]' + editor.selection.getContent() + '[/minus_h]');
+                }
+            });
+            editor.addButton('gray', {
+                image: full + 'gray_icon.png',
+                title: 'Текст на сером фоне',
+                onclick: function () {
+                    editor.insertContent('[gray]' + editor.selection.getContent() + '[/gray]');
+                }
+            });
+            editor.addButton('purple_border', {
+                image: full + 'border_text.png',
+                title: 'Текст с фиолетовой обводкой',
+                onclick: function () {
+                    editor.insertContent('[purple_bd]' + editor.selection.getContent() + '[/purple_bd]');
+                }
+            });
+            editor.addButton('purple_background', {
+                image: full + 'bg_text.png',
+                title: 'Текст на фиолетовом фоне',
+                onclick: function () {
+                    editor.insertContent('[purple_bg]' + editor.selection.getContent() + '[/purple_bg]');
+                }
+            });
+            editor.addButton('gradient_border', {
+                image: full + 'bd_gradient.png',
+                title: 'Текст с градиентоной обводкой',
+                onclick: function () {
+                    editor.insertContent('[gradient_bd]' + editor.selection.getContent() + '[/gradient_bd]');
+                }
+            });
+            editor.addButton('gradient_background', {
+                image: full +  'bg_gradient_u.png',
+                title: 'Текст на градиентоном фоне',
+                onclick: function () {
+                    editor.insertContent('[gradient_bg]' + editor.selection.getContent() + '[/gradient_bg]');
+                }
+            });
+            editor.addButton('light_gradient', {
+                image: full + 'bg_color.png',
+                title: 'Текст на градиентоном фоне',
+                onclick: function () {
+                    editor.insertContent('[gradient_color]' + editor.selection.getContent() + '[/gradient_color]');
+                }
+            });
+            editor.addButton('custom_table', {
+                image: full + 'table_icon.png',
+                title: 'Таблица плюсов и минусов',
+                onclick: function () {
+                    editor.windowManager.open({
+                        title: 'Задайте параметры таблицы',
+                        body: [
+                            {
+                                type: 'textbox', // тип textbox = текстовое поле
+                                name: 'head_plus', // ID, будет использоваться ниже
+                                label: 'Заголовок плюсов', // лейбл
+                                value: '', // значение по умолчанию
+                                autofocus: true,
+                            },
+                            {
+                                type: 'textbox', // тип textbox = текстовое поле
+                                name: 'text_plus',
+                                label: 'Текст плюсов',
+                                value: '<ul>\n<li>List item</li>\n<li>List item</li>\n<li>List item</li>\n</ul>',
+                                multiline: true, // большое текстовое поле - textarea
+                                minWidth: 300, // минимальная ширина в пикселях
+                                minHeight: 150, // минимальная высота в пикселях
+
+                            },
+                            {
+                                type: 'textbox', // тип textbox = текстовое поле
+                                name: 'head_minus', // ID, будет использоваться ниже
+                                label: 'Заголовок минусов', // лейбл
+                                value: '' // значение по умолчанию
+
+                            },
+                            {
+                                type: 'textbox', // тип textbox = текстовое поле
+                                name: 'text_minus',
+                                label: 'Текст минусов',
+                                value: '<ul>\n<li>List item</li>\n<li>List item</li>\n<li>List item</li>\n</ul>',
+                                multiline: true, // большое текстовое поле - textarea
+                                minWidth: 300, // минимальная ширина в пикселях
+                                minHeight: 150, // минимальная высота в пикселях
+
+                            },
+                            {
+                                type: 'textbox', // тип textbox = текстовое поле
+                                name: 'gray',
+                                label: 'Блок с серым фоном',
+                                value: '',
+                                multiline: true, // большое текстовое поле - textarea
+                                minWidth: 300, // минимальная ширина в пикселях
+                                minHeight: 100, // минимальная высота в пикселях
+
+                            },
+
+                        ],
+                        onsubmit: function (e) { // это будет происходить после заполнения полей и нажатии кнопки отправки
+                            editor.insertContent(
+                                '<table cellpadding="0" cellspacing="0" width="100%" class="asl_custom_t">\n' +
+                                '<tbody>\n' +
+                                '<tr>\n' +
+
+                                '<td class="plus">\n' +
+                                '<div class="h3">✅ ' + e.data.head_plus + '</div>\n' +
+                                e.data.text_plus +
+                                '</td>\n' +
+                                '<td class="minus">\n' +
+                                '<div class="h3">⛔' + e.data.head_minus + '</div>\n' +
+                                e.data.text_minus +
+                                '</td>\n' +
+                                '</tr>\n' +
+                                '<tr class="gray_table">\n' +
+                                '<td colspan=2>\n' +
+                                e.data.gray +
+                                '</td>\n' +
+                                '</tr>\n' +
+                                '</tbody>\n' +
+                                '</table>\n');
+                        }
+                    });
+                }
+            });
+        },
+        createControl : function(n, cm) {
+            return null;
+        },
+    });
+    /* Start the buttons */
+    tinymce.PluginManager.add( 'custom_shortcodes', tinymce.plugins.CustomShort );
+})();
